3d278ca71f317f3b836db6ede4c6a128a519b3c1
Nhoppasit S <39229469+nhoppasit@users.noreply.github.com>
2020-05-09 00:38:59 +0700

    Create CB_V1 class

---
+++ src/V1/Arm_App/CB_V1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Bluetooth;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.Util;
+
+namespace Arm_App
+{
+    public class CB_V1
+    {
+        //String a enviar
+        private Java.Lang.String dataToSend;
+        //Variables para el manejo del bluetooth Adaptador y Socket
+        private BluetoothAdapter mBluetoothAdapter = null;
+        private BluetoothSocket btSocket = null;
+        //Streams de lectura I/O
+        private Stream outStream = null;
+        private Stream inStream = null;
+        //MAC Address del dispositivo Bluetooth
+        private static string address = "98:D3:C1:FD:41:51"; //98d3:c1:fd4151
+
+        //Id Unico de comunicacion
+        private static UUID MY_UUID = UUID.FromString("00001101-0000-1000-8000-00805F9B34FB");
+
+        private void CheckBt()
+        {
+            //asignamos el sensor bluetooth con el que vamos a trabajar
+            mBluetoothAdapter = BluetoothAdapter.DefaultAdapter;
+
+            //Verificamos que este habilitado
+            if (!mBluetoothAdapter.Enable())
+            {
+                Toast.MakeText(this, "Bluetooth Desactivado",
+                    ToastLength.Short).Show();
+            }
+            //verificamos que no sea nulo el sensor
+            if (mBluetoothAdapter == null)
+            {
+                Toast.MakeText(this,
+                    "Bluetooth No Existe o esta Ocupado", ToastLength.Short)
+                    .Show();
+            }
+        }
+    }
+}
