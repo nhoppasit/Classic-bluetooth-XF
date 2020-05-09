8f5caa2d8a6e14f626116f1d4f5de4e70d889033
Nhoppasit S <39229469+nhoppasit@users.noreply.github.com>
2020-05-09 23:52:32 +0700

    Create xamarine.forms

--- src/V1/Arm_App/CB_V1.cs
+++ src/V1/Arm_App/CB_V1.cs
@@ -12,43 +12,84 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Java.Util;
+using Newtonsoft.Json;
 
-namespace Arm_App
+namespace ClassicBluetooth
 {
+    public class BaseSimpleReturn
+    {
+        public bool Flag { get; set; }
+        public string Code { get; set; }
+        public string Message { get; set; }
+
+        public BaseSimpleReturn(string code, string message)
+        {
+            Code = code ?? throw new ArgumentNullException(nameof(code));
+            Message = message ?? throw new ArgumentNullException(nameof(message));
+            if (code.Equals("00")) Flag = true;
+            else Flag = false;
+        }
+    }
+
     public class CB_V1
     {
-        //String a enviar
         private Java.Lang.String dataToSend;
-        //Variables para el manejo del bluetooth Adaptador y Socket
+        public Java.Lang.String DataToSend
+        {
+            get { return dataToSend; }
+            set { dataToSend = value; }
+        }
+
         private BluetoothAdapter mBluetoothAdapter = null;
+        public BluetoothAdapter BluetoothAdapter
+        {
+            get { return mBluetoothAdapter; }
+            private set { mBluetoothAdapter = value; }
+        }
+
         private BluetoothSocket btSocket = null;
+        public BluetoothSocket BluetoothSocket
+        {
+            get { return btSocket; }
+            private set { btSocket = value; }
+        }
+
         //Streams de lectura I/O
-        private Stream outStream = null;
+        private Stream outStream = null;    
         private Stream inStream = null;
+
         //MAC Address del dispositivo Bluetooth
         private static string address = "98:D3:C1:FD:41:51"; //98d3:c1:fd4151
 
         //Id Unico de comunicacion
         private static UUID MY_UUID = UUID.FromString("00001101-0000-1000-8000-00805F9B34FB");
 
-        private void CheckBt()
+
+
+        /// <summary>
+        /// ตรวจสอบอุปกรณ์ Bluetooth ของมือถือ
+        /// </summary>
+        /// <returns>BaseSimpleReturn</returns>
+        private BaseSimpleReturn Check()
         {
-            //asignamos el sensor bluetooth con el que vamos a trabajar
             mBluetoothAdapter = BluetoothAdapter.DefaultAdapter;
 
-            //Verificamos que este habilitado
+            /* ---------------------------------------------------------------------------------------
+             * VALIDATION
+             * ---------------------------------------------------------------------------------------*/
             if (!mBluetoothAdapter.Enable())
             {
-                Toast.MakeText(this, "Bluetooth Desactivado",
-                    ToastLength.Short).Show();
+                return new BaseSimpleReturn("01", "Bluetooth ปิดการใช้งาน");
             }
-            //verificamos que no sea nulo el sensor
             if (mBluetoothAdapter == null)
             {
-                Toast.MakeText(this,
-                    "Bluetooth No Existe o esta Ocupado", ToastLength.Short)
-                    .Show();
+                return new BaseSimpleReturn("01", "ไม่พบตัวแปลง Bluetooth");
             }
+
+            /* ---------------------------------------------------------------------------------------
+             * GOOD PASS
+             * ---------------------------------------------------------------------------------------*/
+            return new BaseSimpleReturn("00", "Bluetooth เปิดใช้งานแล้ว");
         }
     }
 }
