4531c0aac46e640a896b1f59f8a841cd48ee6819
Nhoppasit S <39229469+nhoppasit@users.noreply.github.com>
2020-05-10 10:00:19 +0700

    Update CB_V1.cs

--- src/V1/Arm_App/CB_V1.cs
+++ src/V1/Arm_App/CB_V1.cs
@@ -9,6 +9,7 @@ using Android.Bluetooth;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
+using Android.Util;
 using Android.Views;
 using Android.Widget;
 using Java.Util;
@@ -55,16 +56,30 @@ namespace ClassicBluetooth
         }
 
         //Streams de lectura I/O
-        private Stream outStream = null;    
+        private Stream outStream = null;
         private Stream inStream = null;
 
         //MAC Address del dispositivo Bluetooth
-        private static string address = "98:D3:C1:FD:41:51"; //98d3:c1:fd4151
+        private string mAddress = "98:D3:C1:FD:41:51"; //98d3:c1:fd4151
+        public string Address
+        {
+            get { return mAddress; }
+            set { mAddress = value; }
+        }
 
         //Id Unico de comunicacion
-        private static UUID MY_UUID = UUID.FromString("00001101-0000-1000-8000-00805F9B34FB");
+        private  UUID mUuid = UUID.FromString("00001101-0000-1000-8000-00805F9B34FB");
+        public UUID UUID
+        {
+            get { return mUuid; }
+            set { mUuid = value; }
+        }
 
-
+        public CB_V1(string address, string sUuid)
+        {
+            this.mAddress = address;
+            this.mUuid = UUID.FromString(sUuid);
+        }
 
         /// <summary>
         /// ตรวจสอบอุปกรณ์ Bluetooth ของมือถือ
@@ -72,6 +87,7 @@ namespace ClassicBluetooth
         /// <returns>BaseSimpleReturn</returns>
         private BaseSimpleReturn Check()
         {
+            Log.Debug("CB_V1", $"Check adapter");
             mBluetoothAdapter = BluetoothAdapter.DefaultAdapter;
 
             /* ---------------------------------------------------------------------------------------
@@ -91,5 +107,48 @@ namespace ClassicBluetooth
              * ---------------------------------------------------------------------------------------*/
             return new BaseSimpleReturn("00", "Bluetooth เปิดใช้งานแล้ว");
         }
+
+        public void Connect()
+        {
+            //Iniciamos la conexion con el arduino
+            BluetoothDevice device = mBluetoothAdapter.GetRemoteDevice(mAddress);
+
+            System.Console.WriteLine("กำลังเชื่อมต่อ " + device);
+
+            //Indicamos al adaptador que ya no sea visible
+            mBluetoothAdapter.CancelDiscovery();
+            try
+            {
+                //Inicamos el socket de comunicacion con el arduino
+                btSocket = device.CreateRfcommSocketToServiceRecord(mUuid);
+
+                //Conectamos el socket
+                btSocket.Connect();
+                System.Console.WriteLine("Conexion Correcta");
+            }
+            catch (System.Exception e)
+            {
+                //en caso de generarnos error cerramos el socket
+                Console.WriteLine(e.Message);
+                try
+                {
+                    btSocket.Close();
+                }
+                catch (System.Exception)
+                {
+                    System.Console.WriteLine("Imposible Conectar");
+                }
+                System.Console.WriteLine("Socket Creado");
+            }
+            //Una vez conectados al bluetooth mandamos llamar el metodo que generara el hilo
+            //que recibira los datos del arduino
+            beginListenForData();
+            //NOTA envio la letra e ya que el sketch esta configurado para funcionar cuando
+            //recibe esta letra.
+            //  dataToSend = new Java.Lang.String("e");
+            //writeData(dataToSend);
+        }
+
+
     }
 }
